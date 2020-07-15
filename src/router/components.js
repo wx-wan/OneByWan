8f43d93b1cda30832fb1c64580af5f42314bcf62
wx.wan <wx.wan@greatmicro.net>
2020-07-15 14:26:24 +0800

    delete other

--- src/router/components.js
+++
@@ -1,8 +0,0 @@
-export default {
-  Index: () => import('@/views/homepage/index.vue'),
-  Home: () => import('@/views/homepage/Home.vue'),
-  Admin: () => import('@/views/admin/layout'),
-  Cargo: () => import('@/views/cargo'),
-  Foo: { template: '<div>foo</div>' },
-  Bar: { template: '<div>bar</div>' }
-}
